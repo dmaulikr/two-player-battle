75b92fddfaa0e0c523c59278ab7fc1a9fd7d78d4
Chuck Dishmon <chuckdishmon@gmail.com>
2016-05-28 22:28:09 -0400

    Initial Commit

---
+++ two-player-battle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  two-player-battle
+//
+//  Created by Chuck Dishmon on 5/28/16.
+//  Copyright © 2016 Dreison Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
